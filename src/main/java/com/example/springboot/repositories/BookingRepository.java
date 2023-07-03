75db4f7d894e745513ae59cdb65cf4c15ca50108
AndreaBonalumi <andrea.bonalumi@si2001.it>
2023-07-03 17:59:08 +0200

    repositories

---
+++ src/main/java/com/example/springboot/repositories/BookingRepository.java
@@ -0,0 +1,9 @@
+package com.example.springboot.repositories;
+
+import com.example.springboot.entities.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Integer> {
+}
---
+++ src/main/java/com/example/springboot/repositories/CarRepository.java
@@ -0,0 +1,10 @@
+package com.example.springboot.repositories;
+
+import com.example.springboot.entities.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, String> {
+
+}
